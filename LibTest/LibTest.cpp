78c089683ef903362aa648ae4b088588dbfb2be8
ggmini <minigsm14@gmail.com>
2026-06-21 00:03:19 +0200

    feat(testlib): added status output via the testing application to test the new variables

--- LibTest/LibTest.cpp
+++ LibTest/LibTest.cpp
@@ -4,7 +4,10 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <thread>
+#include <iomanip>
 
+const std::string ANSI_RESET_LINE = "\033[2K\033[0G";
 
 std::string getPathFromFile(){
     std::ifstream t("path.txt");
@@ -13,18 +16,38 @@ std::string getPathFromFile(){
     return buffer.str(); 
 }
 
+void printState(maxcsolib::Compressor* c, bool* running) {
+    return;
+    while (*running) {
+        if (c->DataAvailable() == true) {
+            std::cout << ANSI_RESET_LINE << "Percent: " << std::fixed << std::setprecision(0) << c->Percent() << "%, Ratio: " << c->Ratio() << "%, Speed: " << std::setprecision(2) << c->Speed() << " MB/s";
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+}
+
+void runCompressor(maxcsolib::Compressor* c, maxcsolib::Arguments args) {
+    c->Compress(args);
+}
+
 int main() {
     //std::string path = getPathFromFile(); 
     std::string path = "H:\\pulse";
-
     maxcsolib::Arguments args;
     maxcsolib::default_args(args);
-    std::cout << "Converting " << path << ".iso to " << path << ".cso" << std::endl; 
+    std::cout << "Converting " << path << ".iso to " << path << ".cso" << std::endl;
     args.inputs.push_back(path + ".iso");
     args.outputs.push_back(path + ".cso");
-    maxcsolib::Compressor *compressor = new maxcsolib::Compressor();
-    compressor->Compress(args);
+    //args.quiet = true;
+    maxcsolib::Compressor* compressor = new maxcsolib::Compressor();
+
+    bool running = true;
+    std::thread compressor_t(runCompressor, compressor, args);
+    std::thread state_t(printState, compressor, &running);
+    compressor_t.join();
+    running = false;
+    state_t.join();
+
     delete compressor;
-    std::cout << "Hello World!\n";
     return 0;
 }
